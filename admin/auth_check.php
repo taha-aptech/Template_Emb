9a8d3194dba3c61da156f81576fa8e998a9bd492
taha-aptech <taha_abbas@aptechnorth.edu.pk>
2025-07-17 20:28:07 +0500

    Update auth_check.php

--- admin/auth_check.php
+++ admin/auth_check.php
@@ -4,4 +4,14 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
 }
+
+
+
+// Check if user role is admin
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
+    header("Location: ../users/index.php");
+    exit();
+}
+
+
 ?>
